b8b38ea5360964fd2573e4c8ed013bf63b12f891
benattimj <benatti1104@gmail.com>
2024-08-15 14:21:41 -0300

    Construindo aeronaves

--- src/assets/data/blogData.js
+++ src/assets/data/blogData.js
@@ -1,26 +1,26 @@
 // import images from all-images/blog-img directory
-import img01 from "../all-images/blog-img/blog-1.jpg";
-import img02 from "../all-images/blog-img/blog-2.jpg";
-import img03 from "../all-images/blog-img/blog-3.jpg";
+import img01 from "../all-images/plane-img/seneca01.jpg";
+import img02 from "../all-images/plane-img/cesna01.jpg";
+import img03 from "../all-images/plane-img/cessna172.jpg";
 
 const blogData = [
   {
     id: 1,
-    title: "The best way to drive cars",
-    author: "Muhib",
+    title: "Piper Seneca V",
+    author: "Murilo Bj",
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img01,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      "Algumas missões requerem a confiabilidade e redundância de um avião bimotor.  Se uma parte significativa do seu voo for sobre montanhas, à noite, sobre água ou terreno inóspito, considere a tranquilidade que o Piper Seneca V oferece.  Ou você pode estar em um ponto de sua carreira de piloto em que aspira aos desafios de operar um avião multimotor.  Se esses cenários descrevem o seu voo, o Piper Seneca V deve ser o seu avião. O Piper Seneca V também tem desempenho comprovado no negócio de táxi aéreo e fretamento sob demanda. Milhares de Piper Seneca estão em operação em todo o mundo, proporcionando viagens de negócios seguras e confiáveis ​​para aqueles que precisam estar em locais remotos que normalmente não são atendidos pelas companhias aéreas. A configuração de inúmeras portas de entrada e de bagagem com o conforto de um interior de classe cabine faz do Piper Seneca V a ferramenta de negócios perfeita. Velocidade máxima de cruzeiro de 197 ktas. 826 nm em configurações de potência de longo alcance. Altitude operacional de 25.000 pés. O Piper Seneca V oferece o desempenho que você precisa para fornecer transporte pessoal ou viagens de negócios seguros e confortáveis ​​para clientes pagantes",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "Algumas missões requerem a confiabilidade e redundância de um avião bimotor.  Se uma parte significativa do seu voo for sobre montanhas, à noite, sobre água ou terreno inóspito, considere a tranquilidade que o Piper Seneca V oferece.  Ou você pode estar em um ponto de sua carreira de piloto em que aspira aos desafios de operar um avião multimotor.  Se esses cenários descrevem o seu voo, o Piper Seneca V deve ser o seu avião. O Piper Seneca V também tem desempenho comprovado no negócio de táxi aéreo e fretamento sob demanda. Milhares de Piper Seneca estão em operação em todo o mundo, proporcionando viagens de negócios seguras e confiáveis ​​para aqueles que precisam estar em locais remotos que normalmente não são atendidos pelas companhias aéreas. A configuração de inúmeras portas de entrada e de bagagem com o conforto de um interior de classe cabine faz do Piper Seneca V a ferramenta de negócios perfeita. Velocidade máxima de cruzeiro de 197 ktas. 826 nm em configurações de potência de longo alcance. Altitude operacional de 25.000 pés. O Piper Seneca V oferece o desempenho que você precisa para fornecer transporte pessoal ou viagens de negócios seguros e confortáveis ​​para clientes pagantes",
   },
 
   {
     id: 2,
-    title: "If your car battery is down",
-    author: "Muhib",
+    title: "Cessna 152 ",
+    author: "Murilo Bj",
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img02,
@@ -32,8 +32,8 @@ const blogData = [
 
   {
     id: 3,
-    title: "The best way to give trip",
-    author: "Muhib",
+    title: "Cessna 172",
+    author: "Murilo Bj",
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img03,
--- src/components/UI/BecomeDriverSection.jsx
+++ src/components/UI/BecomeDriverSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../styles/become-driver.css";
 import { Container, Row, Col } from "reactstrap";
 
-import driverImg from "../../assets/all-images/toyota-offer-2.png";
+import driverImg from "../../assets/all-images/plane-img/seneca04.jpg";
 
 const BecomeDriverSection = () => {
   return (
@@ -10,12 +10,12 @@ const BecomeDriverSection = () => {
       <Container>
         <Row>
           <Col lg="6" md="6" sm="12" className="become__driver-img">
-            <img src={driverImg} alt="" className="w-100" />
+            <img src={driverImg} alt="" className="w-100"  />
           </Col>
 
           <Col lg="6" md="6" sm="12">
             <h2 className="section__title become__driver-title">
-              Do You Want to Earn With Us? So Don't Be Late
+            Faça parte da aviação
             </h2>
 
             <button className="btn become__driver-btn mt-4">
