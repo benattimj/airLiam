5c69f1117af2a70bbbd75b02c6f443d71f754501
Murilo Benatti <benatti1104@gmail.com>
2024-08-15 21:22:37 -0300

    Aeronaves

--- src/assets/data/blogData.js
+++ src/assets/data/blogData.js
@@ -1,7 +1,10 @@
-// import images from all-images/blog-img directory
 import img01 from "../all-images/plane-img/seneca01.jpg";
 import img02 from "../all-images/plane-img/cesna01.jpg";
 import img03 from "../all-images/plane-img/cessna172.jpg";
+import img04 from "../all-images/plane-img/king350.webp";
+import img05 from "../all-images/plane-img/g650.jpg";
+import img06 from "../all-images/plane-img/p12.jpg";
+
 
 const blogData = [
   {
@@ -11,20 +14,24 @@ const blogData = [
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img01,
-    description: "Com seu desempenho robusto e a capacidade de atender a necessidades de transporte pessoal e negócios,",
-    quote:`<p>O <strong>Cessna 152</strong> é um avião leve ideal para treinamento e voos de lazer. Com um design simples e confiável, é perfeito para pilotos iniciantes e voos curtos.</p>
-<p>Este modelo é conhecido por sua facilidade de manuseio e eficiência, tornando-se uma escolha popular para escolas de aviação e entusiastas da aviação leve.</p>
-<h3>Características Principais:</h3>
-<ul class="aircraft-features">
-  <li><strong>Velocidade Máxima de Cruzeiro:</strong> Aproximadamente 150 km/h</li>
-  <li><strong>Alcance:</strong> Aproximadamente 800 km</li>
-  <li><strong>Altitude Operacional:</strong> Até 12.000 pés</li>
-  <li><strong>Configuração:</strong> 2 assentos</li>
-  <li><strong>Interior:</strong> Simples e funcional</li>
-  <li><strong>Tipo de Motor:</strong> Motor de um único cilindro</li>
-</ul>
-<p>Com uma excelente reputação por sua confiabilidade e simplicidade, o Cessna 152 é uma escolha excelente para quem está começando na aviação.</p>
-    `
+    description: `
+      O <strong>Piper Seneca V</strong> é uma aeronave bimotor de alta performance, ideal para voos de longa distância e transporte executivo. Com uma cabine espaçosa e moderna, é uma escolha popular para quem busca conforto e eficiência.`,
+    specifications: {
+      velocidadeMaxima: "200 nós",
+      alcance: "1.000 milhas náuticas",
+      altitudeMaxima: "25.000 pés",
+      capacidadeCombustivel: "123 galões",
+      numeroAssentos: 6,
+      tipoMotor: "2x Continental TSIO-360-RB",
+    },
+    manufacturerInfo: {
+      fabricante: "Piper Aircraft",
+      paisOrigem: "Estados Unidos",
+      anoFabricacao: "1998 - presente",
+    },
+    quote: `
+      <p>O Piper Seneca V oferece o equilíbrio perfeito entre desempenho e conforto. Sua capacidade de operar em pistas curtas e em condições meteorológicas adversas o torna uma escolha versátil para diversas missões.</p>
+    `,
   },
   {
     id: 2,
@@ -33,12 +40,25 @@ const blogData = [
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img02,
-    description:
-      " O Cessna 152 é um dos aviões de treinamento mais icônicos e confiáveis do mundo da aviação geral. ",
-    quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+    description: `
+      O <strong>Cessna 152</strong> é um avião leve ideal para treinamento e voos de lazer. Com um design simples e confiável, é perfeito para pilotos iniciantes.`,
+    specifications: {
+      velocidadeMaxima: "107 nós",
+      alcance: "415 milhas náuticas",
+      altitudeMaxima: "14.700 pés",
+      capacidadeCombustivel: "26 galões",
+      numeroAssentos: 2,
+      tipoMotor: "Lycoming O-235",
+    },
+    manufacturerInfo: {
+      fabricante: "Cessna Aircraft",
+      paisOrigem: "Estados Unidos",
+      anoFabricacao: "1977 - 1985",
+    },
+    quote: `
+      <p>Com uma excelente reputação por sua confiabilidade e simplicidade, o Cessna 152 é uma escolha excelente para quem está começando na aviação.</p>
+    `,
   },
-
   {
     id: 3,
     title: "Cessna 172",
@@ -46,28 +66,105 @@ const blogData = [
     date: "12 Dec, 2020",
     time: "9pm",
     imgUrl: img03,
-    description:`
-      <div>
-        <p>
-          O <strong>Cessna 172 Skyhawk</strong> é um dos aviões mais populares e amplamente utilizados na aviação geral. Desde seu lançamento na década de 1950, o Cessna 172 tem sido uma escolha preferida tanto para treinamento de pilotos quanto para voos recreativos, graças à sua confiabilidade, facilidade de manejo e versatilidade.
-        </p>
-        <p>
-          <strong>Características Principais:</strong>
-        </p>
-        <ul class="aircraft-features">
-          <li><strong>Design Robusto:</strong> A estrutura do Cessna 172 é conhecida por sua durabilidade e resistência, tornando-o ideal para uma ampla gama de condições meteorológicas e operações.</li>
-          <li><strong>Desempenho de Voo:</strong> O Cessna 172 oferece um desempenho confiável com uma velocidade de cruzeiro típica de aproximadamente 122 nós e um alcance de cerca de 640 milhas náuticas.</li>
-          <li><strong>Facilidade de Treinamento:</strong> Com sua configuração simples e estável, o Cessna 172 é um excelente avião para pilotos em treinamento, proporcionando uma experiência de voo intuitiva e segura.</li>
-          <li><strong>Interior Confortável:</strong> O cockpit espaçoso e a cabine bem equipada garantem conforto para pilotos e passageiros durante o voo.</li>
-          <li><strong>Manutenção e Operação:</strong> O Cessna 172 é conhecido por suas baixas necessidades de manutenção e custos operacionais acessíveis, o que o torna uma escolha econômica para escolas de aviação e operadores privados.</li>
-        </ul>
-        <p>
-          A confiabilidade do Cessna 172 e sua versatilidade em diferentes cenários fazem dele uma escolha ideal para uma variedade de missões, desde treinamento até transporte leve e voos recreativos. Sua popularidade contínua ao longo dos anos é um testemunho de seu design bem-sucedido e desempenho consistente.
-        </p>
-      </div>
-    `, quote:
-      "O Cessna 172 Skyhawk é um dos aviões mais populares e amplamente utilizados na aviação geral. Desde o seu lançamento na década de 1950, o Cessna 172 tem sido uma escolha preferida tanto para treinamento de pilotos quanto para voos recreativos, graças à sua confiabilidade, facilidade de manejo e versatilidade.",
+    description: `
+      O <strong>Cessna 172 Skyhawk</strong> é um dos aviões mais populares na aviação geral. Sua confiabilidade e facilidade de manejo o tornam ideal para treinamento e voos recreativos.`,
+    specifications: {
+      velocidadeMaxima: "122 nós",
+      alcance: "640 milhas náuticas",
+      altitudeMaxima: "13.500 pés",
+      capacidadeCombustivel: "56 galões",
+      numeroAssentos: 4,
+      tipoMotor: "Lycoming O-320",
+    },
+    manufacturerInfo: {
+      fabricante: "Cessna Aircraft",
+      paisOrigem: "Estados Unidos",
+      anoFabricacao: "1956 - presente",
+    },
+    quote: `
+      <p>O Cessna 172 é uma escolha popular tanto para treinamento de pilotos quanto para voos recreativos, graças à sua confiabilidade e versatilidade.</p>
+    `,
   },
+  {
+    id: 4,
+    title: "Beechcraft King Air 350",
+    author: "Murilo Bj",
+    date: "15 Jan, 2021",
+    time: "3pm",
+    imgUrl: img04,
+    description: `
+      O <strong>Beechcraft King Air 350</strong> é uma aeronave bimotor turboélice conhecida por sua versatilidade e confiabilidade. É amplamente utilizado para transporte executivo e voos charter devido à sua capacidade de operar em pistas curtas.`,
+    specifications: {
+      velocidadeMaxima: "312 nós",
+      alcance: "1.806 milhas náuticas",
+      altitudeMaxima: "35.000 pés",
+      capacidadeCombustivel: "361 galões",
+      numeroAssentos: 11,
+      tipoMotor: "2x Pratt & Whitney PT6A-60A",
+    },
+    manufacturerInfo: {
+      fabricante: "Beechcraft",
+      paisOrigem: "Estados Unidos",
+      anoFabricacao: "1984 - presente",
+    },
+    quote: `
+      <p>O King Air 350 é um dos aviões mais versáteis em sua classe, combinando desempenho superior com conforto para os passageiros.</p>
+    `,
+  },
+  {
+    id: 5,
+    title: "Gulfstream G650",
+    author: "Murilo Bj",
+    date: "22 Feb, 2021",
+    time: "1pm",
+    imgUrl: img05,
+    description: `
+      O <strong>Gulfstream G650</strong> é uma aeronave de ultralongo alcance conhecida por seu luxo e tecnologia de ponta. É amplamente utilizado por executivos e celebridades para viagens internacionais.`,
+    specifications: {
+      velocidadeMaxima: "516 nós",
+      alcance: "7.000 milhas náuticas",
+      altitudeMaxima: "51.000 pés",
+      capacidadeCombustivel: "44.200 libras",
+      numeroAssentos: 18,
+      tipoMotor: "2x Rolls-Royce BR725",
+    },
+    manufacturerInfo: {
+      fabricante: "Gulfstream Aerospace",
+      paisOrigem: "Estados Unidos",
+      anoFabricacao: "2008 - presente",
+    },
+    quote: `
+      <p>O Gulfstream G650 é o epítome do luxo nos céus, oferecendo alcance inigualável e conforto sem comprometer o desempenho.</p>
+    `,
+  },
+  {
+    id: 6,
+    title: "Pilatus PC-12",
+    author: "Murilo Bj",
+    date: "10 Mar, 2021",
+    time: "4pm",
+    imgUrl: img06,
+    description: `
+      O <strong>Pilatus PC-12</strong> é uma aeronave monomotor turboélice versátil e confiável, ideal para operações em pistas curtas e não pavimentadas. É popular tanto para transporte executivo quanto para missões especiais.`,
+    specifications: {
+      velocidadeMaxima: "285 nós",
+      alcance: "1.845 milhas náuticas",
+      altitudeMaxima: "30.000 pés",
+      capacidadeCombustivel: "402 galões",
+      numeroAssentos: 9,
+      tipoMotor: "Pratt & Whitney PT6A-67P",
+    },
+    manufacturerInfo: {
+      fabricante: "Pilatus Aircraft",
+      paisOrigem: "Suíça",
+      anoFabricacao: "1994 - presente",
+    },
+    quote: `
+      <p>Com uma combinação única de desempenho, economia e flexibilidade, o Pilatus PC-12 é um dos aviões mais procurados no mercado de aviação geral.</p>
+    `,
+  },
+
+
 ];
 
 export default blogData;
--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from "react";
 import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
-
 import { useParams } from "react-router-dom";
 import blogData from "../assets/data/blogData.js";
 import Helmet from "../components/Helmet/Helmet";
 import { Link } from "react-router-dom";
 
 import commentImg from "../assets/all-images/ava-1.jpg";
-
 import "../styles/blog-details.css";
 
 const BlogDetails = () => {
@@ -18,6 +16,24 @@ const BlogDetails = () => {
     window.scrollTo(0, 0);
   }, [blog]);
 
+  // Verificação se o blog existe
+  if (!blog) {
+    return (
+      <Helmet title="Not Found">
+        <section>
+          <Container>
+            <Row>
+              <Col>
+                <h2 className="section__title">Postagem não encontrada</h2>
+                <p>Desculpe, não conseguimos encontrar a aeronave que você está procurando.</p>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </Helmet>
+    );
+  }
+
   return (
     <Helmet title={blog.title}>
       <section>
@@ -25,7 +41,7 @@ const BlogDetails = () => {
           <Row>
             <Col lg="8" md="8">
               <div className="blog__details">
-                <img src={blog.imgUrl} alt="" className="w-100" />
+                <img src={blog.imgUrl} alt={blog.title} className="w-100" />
                 <h2 className="section__title mt-4">{blog.title}</h2>
 
                 <div className="blog__publisher d-flex align-items-center gap-4 mb-4">
@@ -34,27 +50,44 @@ const BlogDetails = () => {
                   </span>
 
                   <span className=" d-flex align-items-center gap-1 section__description">
-                    <i class="ri-calendar-line"></i> {blog.date}
+                    <i className="ri-calendar-line"></i> {blog.date}
                   </span>
 
                   <span className=" d-flex align-items-center gap-1 section__description">
-                    <i class="ri-time-line"></i> {blog.time}
+                    <i className="ri-time-line"></i> {blog.time}
                   </span>
                 </div>
 
-                <div 
-                  className="section__description" 
-                  dangerouslySetInnerHTML={{ __html: blog.quote }} 
+                <div
+                  className="section__description"
+                  dangerouslySetInnerHTML={{ __html: blog.description }}
                 />
-                <h6 className="ps-5 fw-normal">
-                </h6>
+                <div className="section__description mt-4">
+                  <h5>Especificações:</h5>
+                  <ul>
+                    <li><strong>Velocidade Máxima:</strong> {blog.specifications.velocidadeMaxima}</li>
+                    <li><strong>Alcance:</strong> {blog.specifications.alcance}</li>
+                    <li><strong>Altitude Máxima:</strong> {blog.specifications.altitudeMaxima}</li>
+                    <li><strong>Capacidade de Combustível:</strong> {blog.specifications.capacidadeCombustivel}</li>
+                    <li><strong>Número de Assentos:</strong> {blog.specifications.numeroAssentos}</li>
+                    <li><strong>Tipo de Motor:</strong> {blog.specifications.tipoMotor}</li>
+                  </ul>
+                </div>
+                <div className="section__description mt-4">
+                  <h5>Informações do Fabricante:</h5>
+                  <ul>
+                    <li><strong>Fabricante:</strong> {blog.manufacturerInfo.fabricante}</li>
+                    <li><strong>País de Origem:</strong> {blog.manufacturerInfo.paisOrigem}</li>
+                    <li><strong>Ano de Fabricação:</strong> {blog.manufacturerInfo.anoFabricacao}</li>
+                  </ul>
+                </div>
               </div>
 
               <div className="comment__list mt-5">
-                <h4 className="mb-5">3 Comments</h4>
+                <h4 className="mb-5">3 Comentários</h4>
 
                 <div className="single__comment d-flex gap-3">
-                  <img src={commentImg} alt="" />
+                  <img src={commentImg} alt="Comentário" />
                   <div className="comment__content">
                     <h6 className=" fw-bold">David Visa</h6>
                     <p className="section__description mb-0">14 July, 2022</p>
@@ -65,21 +98,20 @@ const BlogDetails = () => {
                     </p>
 
                     <span className="replay d-flex align-items-center gap-1">
-                      <i class="ri-reply-line"></i> Replay
+                      <i className="ri-reply-line"></i> Responder
                     </span>
                   </div>
                 </div>
 
-                {/* =============== comment form ============ */}
                 <div className="leave__comment-form mt-5">
-                  <h4>Leave a Comment</h4>
+                  <h4>Deixe um Comentário</h4>
                   <p className="section__description">
-                    You must sign-in to make or comment a post
+                    Você deve estar logado para fazer um comentário
                   </p>
 
                   <Form>
                     <FormGroup className=" d-flex gap-3">
-                      <Input type="text" placeholder="Full name" />
+                      <Input type="text" placeholder="Nome completo" />
                       <Input type="email" placeholder="Email" />
                     </FormGroup>
 
@@ -87,12 +119,12 @@ const BlogDetails = () => {
                       <textarea
                         rows="5"
                         className="w-100 py-2 px-3"
-                        placeholder="Comment..."
+                        placeholder="Comentário..."
                       ></textarea>
                     </FormGroup>
 
                     <button className="btn comment__btn mt-3">
-                      Post a Comment
+                      Postar Comentário
                     </button>
                   </Form>
                 </div>
@@ -101,14 +133,14 @@ const BlogDetails = () => {
 
             <Col lg="4" md="4">
               <div className="recent__post mb-4">
-                <h5 className=" fw-bold">Recent Posts</h5>
+                <h5 className=" fw-bold">Postagens Recentes</h5>
               </div>
               {blogData.map((item) => (
                 <div className="recent__blog-post mb-4" key={item.id}>
                   <div className="recent__blog-item d-flex gap-3">
-                    <img src={item.imgUrl} alt="" className="w-25 rounded-2" />
+                    <img src={item.imgUrl} alt={item.title} className="w-25 rounded-2" />
                     <h6>
-                      <Link to={`/blogs/${item.title}`}>{blog.title}</Link>
+                      <Link to={`/blogs/${item.title}`}>{item.title}</Link>
                     </h6>
                   </div>
                 </div>
